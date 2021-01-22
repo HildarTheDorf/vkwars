1d2e61a324493aa5585762a0b585cc989debef7e
Gareth Webb <gareth.webb@umbralsoftware.co.uk>
2021-01-22 22:12:48 +0000

    Split glfw code to new file

--- main.cpp
+++ main.cpp
@@ -1,40 +1,22 @@
 #include "Renderer.hpp"
-
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+#include "Window.hpp"
 
 #include "imgui.h"
-#include "imgui_impl_glfw.h"
-
-struct GlfwDeleter
-{
-    void operator()(GLFWwindow *window)
-    {
-        glfwDestroyWindow(window);
-    }
-};
 
 int main()
 {
     ImGui::CreateContext();
     ImGui::GetIO().FontGlobalScale *= 2;
 
-    glfwInit();
+    Window window;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
-    const auto window = std::unique_ptr<GLFWwindow, GlfwDeleter>(glfwCreateWindow(800, 600, "vkwars", nullptr, nullptr));
-
-    ImGui_ImplGlfw_InitForVulkan(window.get(), true);
-
-    Renderer renderer(glfwGetRequiredInstanceExtensions, [&window](VkInstance instance, VkAllocationCallbacks *allocator, VkSurfaceKHR *pSurface){
-        return glfwCreateWindowSurface(instance, window.get(), allocator, pSurface);
+    Renderer renderer([&window](uint32_t *pCount){ return window.getVulkanExtensions(pCount); }, [&window](VkInstance instance, VkAllocationCallbacks *allocator, VkSurfaceKHR *pSurface){
+        return window.getVulkanSurface(instance, allocator, pSurface);
     });
 
-    while (!glfwWindowShouldClose(window.get()))
+    while (!window.shouldClose())
     {
-        glfwPollEvents();
-        ImGui_ImplGlfw_NewFrame();
+        Window::PollEvents();
 
         ImGui::NewFrame();
         ImGui::ShowDemoWindow();
@@ -44,6 +26,5 @@ int main()
         renderer.render();
     }
 
-    ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
 }
